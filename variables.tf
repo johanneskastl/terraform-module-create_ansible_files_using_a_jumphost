87c98e09fd53fda404a75f2099db0fb897a63d32
Johannes Kastl <kastl@b1-systems.de>
2021-03-14 15:32:52 +0100

    ADD variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+#
+# Ansible inventory
+#
+
+variable "file_name_with_path" {
+  description = "File name for the new file"
+  default     = "ansible/terraform_ansible_inventory"
+}
+
+variable "file_permissions" {
+  description = "Permissions for the new file"
+  default     = "0644"
+}
+
+variable "groups_with_floating_ips" {
+  description = "Instance objects created using the create_instance_with_floating_ip module"
+  type        = list
+}
+
+variable "groups_without_floating_ips" {
+  description = "Instance objects created using the terraform-module-create_instance_without_floating_ip module"
+  type        = list
+}
